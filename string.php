11a096293eb266d5578d1ac4a54b97a567aa81ae
Aldin Wildan <08.aldinwildan@gmail.com>
2024-04-03 14:00:24 +0700

    mengerjakan keseluruhan soal string untuk no 2 dan no 3

--- string.php
+++ string.php
@@ -27,6 +27,35 @@
     echo "Panjang string : $panjang_second karakter <br>Jumlah kata : $jumlah_second <br>";
     ?>
     <hr>
+    <?php
+    echo "<h3>Soal No 2</h3>";
+    /* 
+        SOAL NO 2
+        Mengambil kata pada string dan karakter-karakter yang ada di dalamnya.        
+    */
+    $string2 = "I love PHP";
+    echo "<label>String: </label> \"$string2\" <br>";
+    echo "Kata pertama: " . substr($string2, 0, 1) . "<br>";
+    echo "Kata kedua: " . substr($string2, 2, 4) . "<br>";
+    echo "Kata ketiga: " . substr($string2, 7, 3) . "<br>";
+    ?>
+    <hr>
+    <?php
+    echo "<h3> Soal No 3 </h3>";
+    /*
+        SOAL NO 3
+        Mengubah karakter atau kata yang ada di dalam sebuah string.
+    */
+    $string3 = "PHP is old but sexy!";
+    echo "string sebelum diubah: " . "<br>";
+    echo "String: \"$string3\" ";
+    echo "<br><br>";
+    $ambil = substr($string3, 0, 15);
+    $baru = $ambil . "awesome";
+    echo "string setelah diubah: " . "<br>";
+    echo "String: \"$baru\" ";
+
+    ?>
 </body>
 
 </html>
